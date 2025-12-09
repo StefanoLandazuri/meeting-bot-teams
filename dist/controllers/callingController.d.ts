ea0ccef5bb80d7c3879077e4d5860ef709edbe67
Stefano Landazuri <slandazuri2003g@gmail.com>
2025-12-09 16:36:37 -0500

    Delete dist directory

--- dist/controllers/callingController.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express';
-export declare const handleCallingWebhook: (req: Request, res: Response) => Promise<void>;
-export declare const handleJoinMeeting: (req: Request, res: Response) => Promise<void>;
-export declare const handleProcessTranscript: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=callingController.d.ts.map
